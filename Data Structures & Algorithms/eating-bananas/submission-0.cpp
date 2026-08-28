88c59532489fa134e18c81f00f6006aa1856f391
iambhaveshrajput <bhaveshrajput4321@gmail.com>
2026-08-28 08:59:47 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = *std::max_element(piles.begin(), piles.end());
+        int ans = high;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long long totalHours = 0;
+            for (int pile : piles) {
+                totalHours += (pile + mid - 1LL) / mid;
+            }
+            if (totalHours <= h) {
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+
+        }
+        return ans;
+    }
+};
